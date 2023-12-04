a0170964ca09a7fcb3569b57d262eea8f6d121fe
Michael Louis Langan <20106749@mail.wit.ie>
2023-12-04 11:54:45 +0000

    Assignment 2 - 2023-12-04 ================================================== TeamSys - Player - name ================================================== + name + getName() + setName()

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,17 @@
 public class Player {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.length() <= 20) {
+            this.name = name;
+        }
+        else {
+            this.name = name.substring(0, 19);
+        }
+    }
 }
