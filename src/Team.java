64dcd043b34b73b66f110c771b5fa1912f89cda2
Michael Louis Langan <20106749@mail.wit.ie>
2023-12-07 15:32:41 +0000

    Assignment 2 - 2023-12-07 ================================================== TeamSys - Team - players ================================================== + Player[] + getPlayers() + setPlayers() //doesnt work yet

--- src/Team.java
+++ src/Team.java
@@ -1,2 +1,11 @@
 public class Team {
+    Player[] players;
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players, int ) {
+        ;
+    }
 }
